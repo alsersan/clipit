4967bd312451c8aae5dd898f668e128110d3df1a
Álvaro Serrano <alsersan20@gmail.com>
2022-04-12 12:38:38 +0200

    Add logic to close modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ModalComponent {
+  @Output() closeModal = new EventEmitter();
 }
