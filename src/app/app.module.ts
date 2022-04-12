d425340abea13920a461ba7d0a6c715085174c08
Álvaro Serrano <alsersan20@gmail.com>
2022-04-12 10:53:15 +0200

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
 import { CardComponent } from './card/card.component';
 import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HeroComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
+
+
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, SharedModule],
+  imports: [
+    CommonModule
+  ]
 })
-export class UserModule {}
+export class SharedModule { }
