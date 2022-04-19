df23944abba203db3fb15ab2dfc34f30309f0514
Álvaro Serrano <alsersan20@gmail.com>
2022-04-19 12:29:06 +0200

    Creaete TabContainerComponent in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabComponent } from './tab/tab.component';
+import { TabContainerComponent } from './tab-container/tab-container.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabComponent],
+  declarations: [ModalComponent, TabComponent, TabContainerComponent],
   imports: [CommonModule],
-  exports: [ModalComponent, TabComponent],
+  exports: [ModalComponent, TabComponent, TabContainerComponent],
 })
 export class SharedModule {}
