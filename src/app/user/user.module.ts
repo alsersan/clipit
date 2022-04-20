34a58c59ef120d8d69bcc21432ab421a70bd710e
Álvaro Serrano <alsersan20@gmail.com>
2022-04-20 14:01:14 +0200

    Generate login component in user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AuthModalComponent],
+  declarations: [AuthModalComponent, LoginComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
