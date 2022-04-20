07877ca1c834e72544d00ba84b3aa9495b925b00
Álvaro Serrano <alsersan20@gmail.com>
2022-04-20 14:07:13 +0200

    Generate registerComponent in user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AuthModalComponent, LoginComponent],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent, LoginComponent],
 })
